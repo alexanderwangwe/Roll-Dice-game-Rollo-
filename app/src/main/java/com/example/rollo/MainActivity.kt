50ce5d902126151416f4ca8194b2f685c80830ae
alexw <alexanderwangwe@gmail.com>
2023-11-08 15:34:07 +0300

    added images and app logic

--- app/src/main/java/com/example/rollo/MainActivity.kt
+++ app/src/main/java/com/example/rollo/MainActivity.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rollo.ui.theme.RolloTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,29 +37,39 @@ class MainActivity : ComponentActivity() {
             RolloTheme {
                 DiceRollerApp()
             }
-            }
         }
+    }
 }
 
 
 @Preview
 @Composable
 fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 
 }
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier= Modifier
     .fillMaxSize()
-    .wrapContentSize(Alignment.Center))
-{
+    .wrapContentSize(Alignment.Center)) {
+    var result by remember { mutableStateOf(1) }
+    var imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-            Text(text = stringResource(id = R.string.roll))
+        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(text = stringResource(R.string.roll))
         }
-
     }
-// adding roll button
+}
